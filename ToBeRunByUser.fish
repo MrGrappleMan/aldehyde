f92737b1a6322432e489f74f082c99ba67bc65d7
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-01-18 21:47:14 +0530

    user runnable

---
+++ ToBeRunByUser.fish
@@ -0,0 +1,13 @@
+#!/bin/fish
+
+# ⛑️ Remove compatibility
+rpm-ostree cancel -q
+rpm-ostree reset -l -o -i -q
+ostree admin pin -u 0
+ostree admin pin -u 1
+ostree admin pin -u 2
+ostree admin pin -u 3
+ostree admin pin -u 4
+
+# 🎛️ Switch bootc base to Aldehyde
+bootc switch ghcr.io/MrGrappleMan/aldehyde-lx:latest --quiet
