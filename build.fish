4b293af3963faff0a2bd9298a0f945f356f32ec2
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-01-21 14:49:34 +0530

    Update build.fish

--- build.fish
+++ build.fish
@@ -19,10 +19,10 @@ dnf5 install fish -y
 # === === /ctx/script/ subscripts === ===
 echo --- Run subscripts ---
 
-fish /ctx/script/bootc.fish
+#fish /ctx/script/bootc.fish
 fish /ctx/script/dnf5.fish
-fish /ctx/script/flatpak.fish
+#fish /ctx/script/flatpak.fish
 fish /ctx/script/systemd.fish
-fish /ctx/script/fwupdmgr.fish
+#fish /ctx/script/fwupdmgr.fish
 #fish /ctx/script/dconf.fish
 #fish /ctx/script/ujust.fish
