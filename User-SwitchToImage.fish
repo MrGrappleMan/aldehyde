be3487db566f207b921ed16a29708aadcf9346fc
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-02-06 15:45:21 +0530

    reduce potential bloat

--- User-SwitchToImage.fish
+++ User-SwitchToImage.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/env fish
+clear
 
 if test (id -u) -eq 0
     echo "Running as root, proceeding"
 else
-    echo "Not running as root, beginning re-execute..."
+    echo "Not running as root, beginning re-execution!"
     echo "Please insert the proper password for your user"
     sleep 3
     curl "https://raw.githubusercontent.com/MrGrappleMan/aldehyde-lx/refs/heads/main/User-SwitchToImage.fish" | pkexec fish
@@ -25,6 +26,19 @@ ostree admin pin -u 3
 ostree admin pin -u 4
 
 # 🎛️ Switch image
-echo "Switching to new image"
+clear
+echo "   ▄████████  ▄█       ████████▄     ▄████████    ▄█    █▄    ▄██   ▄   ████████▄     ▄████████ ";
+echo "  ███    ███ ███       ███   ▀███   ███    ███   ███    ███   ███   ██▄ ███   ▀███   ███    ███ ";
+echo "  ███    ███ ███       ███    ███   ███    █▀    ███    ███   ███▄▄▄███ ███    ███   ███    █▀  ";
+echo "  ███    ███ ███       ███    ███  ▄███▄▄▄      ▄███▄▄▄▄███▄▄ ▀▀▀▀▀▀███ ███    ███  ▄███▄▄▄     ";
+echo "▀███████████ ███       ███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀▀███▀  ▄██   ███ ███    ███ ▀▀███▀▀▀     ";
+echo "  ███    ███ ███       ███    ███   ███    █▄    ███    ███   ███   ███ ███    ███   ███    █▄  ";
+echo "  ███    ███ ███▌    ▄ ███   ▄███   ███    ███   ███    ███   ███   ███ ███   ▄███   ███    ███ ";
+echo "  ███    █▀  █████▄▄██ ████████▀    ██████████   ███    █▀     ▀█████▀  ████████▀    ██████████ ";
+echo "             ▀                                                                                  ";
+echo "Improvise, Perform, Inspire";
+echo
+echo "❇ Preparing your experience ❇";
+echo "Please be patient, your device will automatically reboot";
 bootc switch ghcr.io/mrgrappleman/aldehyde-lx:latest --apply
-bootc update
+bootc upgrade --apply
