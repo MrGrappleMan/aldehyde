c36a13a10d1fd1271559b0188e59294aa1a68807
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-02-22 23:38:59 +0530

    Update dnf5.fish

--- script/dnf5.fish
+++ script/dnf5.fish
@@ -32,9 +32,8 @@ echo "⭕ --- (-) Delete packages ---"
 
 sysPkg- docker docker-compose moby-engine \
         firefox \
-        code
-
-#gnome-shell gdm mutter gnome-session gnome-control-center gnome-initial-setup
+        code \
+        gnome-shell gdm mutter gnome-session gnome-control-center gnome-initial-setup
 
 # (~) PKG SWAP
 echo "⭕ --- (~) Swap packages ---"
