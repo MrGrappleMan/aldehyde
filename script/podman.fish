8667238e64b0b0c0c3a155b58a6c686c670b86e3
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-02-23 12:52:29 +0530

    Switch to podman

--- script/podman.fish
+++ script/podman.fish
@@ -1,15 +1,17 @@
-#!/bin/env fish
+#!/usr/bin/env fish
 
-# Docker
-# !`Setup----------
-for cpkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker container runc; do sudo apt-fast remove $cpkg; done
-cd /tmp/
-curl -fsSL https://test.docker.com -o dck.sh
-sudo sh dck.sh
-# !`Final----------
-CONTAINERS='mgmpsclient mgmearnfm-client honeygain repocket pawns-cli earnfm-client psclient'
+# Honeygain
+podman pull docker.io/honeygain/honeygain
+podman run --rm honeygain/honeygain -tou-get
+podman run -d --restart always --name honeygain_node \
+  honeygain/honeygain -tou-accept \
+  -email ACCOUNT_EMAIL -pass ACCOUNT_PASSWORD -device DEVICE_NAME
+
+# Podman
+CONTAINERS='aldy-packetshare aldy-earnfm honeygain repocket pawns-cli earnfm-client psclient'
 sudo docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --include-restarting --revive-stopped --interval 300 $CONTAINERS
 sudo docker update --restart=always --memory-swap=-1 --cpus=0.000 --cpu-quota=0 --pids-limit=-1 --cpu-rt-period=2000000 $(sudo docker ps -q -a)
-# !`Remove all(For emergencies)----------
-sudo docker rm $(sudo docker ps -q -a) -f
 
+# Remove all containers/pods, not images
+#podman rm -af
+#podman pod rm -af
